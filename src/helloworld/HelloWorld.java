4082809c1dbded80a87ac99690f14bb95e49f6b8
weiji1726 <34057089+weiji1726@users.noreply.github.com>
2018-01-30 15:27:33 +0800

    Delete HelloWorld.java

--- src/helloworld/HelloWorld.java
+++
@@ -1,9 +0,0 @@
-package helloworld;
-
-public class HelloWorld {
-
-    public static void main(String[] args) {
-        System.out.println("Hello World 再一次加强版~");
-        System.out.println("城阙普三秦");
-    }
-}
